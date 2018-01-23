c6914833a3f1d7c1a1805a2145810971cd6aed77
Wilson Wang <wilsonw926@gmail.com>
2018-01-23 01:42:04 -0800

    basic migrations

---
+++ db/migrate/20180123091951_initial_schema.rb
@@ -0,0 +1,17 @@
+class InitialSchema < ActiveRecord::Migration[5.1]
+    def change
+
+	    create_table :belts do |t|
+	  		t.string :color
+	  	end
+
+	  	create_table :techniques do |t|
+	  		t.string :name
+	  		t.string :difficulty
+	  		t.text :notes
+	  		t.string :martial_art
+	  		t.string :type
+	  	end
+
+  	end
+end
