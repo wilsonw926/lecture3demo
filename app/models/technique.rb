9fbdcbca0453fdd73a7d7ddbd3098411d229bbf4
Wilson Wang <wilsonw926@gmail.com>
2018-01-25 15:12:45 -0800

    css

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -1,7 +1,7 @@
 class Technique < ApplicationRecord
 
 	validates_presence_of :name
-	validates_presence_of :difficulty
+	validates_presence_of :difficulty	
 	validates_presence_of :martial_art
 	validates_presence_of :form_type
 	validates_presence_of :belt_color
