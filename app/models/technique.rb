4bc79b16bbe4b1774ff458cf687f7d8e4bd3154a
Wilson Wang <wilsonw926@gmail.com>
2018-01-24 23:59:26 -0800

    made notes and video link optional, centered add technique button

--- app/models/technique.rb
+++ app/models/technique.rb
@@ -2,10 +2,8 @@ class Technique < ApplicationRecord
 
 	validates_presence_of :name
 	validates_presence_of :difficulty
-	validates_presence_of :notes
 	validates_presence_of :martial_art
 	validates_presence_of :form_type
-	validates_presence_of :link
 	validates_presence_of :belt_color
 
 end
